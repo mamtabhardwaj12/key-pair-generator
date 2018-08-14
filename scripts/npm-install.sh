12f3dd4a240900194c6e2519ccf5dc096108f673
root <root@ip-172-31-44-173.us-west-2.compute.internal>
2018-08-14 14:41:35 +0530

    Fifth commit

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/lendledger/key-pair-generator
-npm install
+sudo npm install
--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ubuntu/lendledger/key-pair-generator
-npm start
+sudo npm start
